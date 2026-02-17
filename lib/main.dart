d0254652010e0a17585f57f79b11686762d7995b
shahpalsha <shahpalsha@gmail.com>
2026-02-17 15:43:57 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Floating Action Button Library"),
+        title: const Text("Floating Action Button"),
         centerTitle: true,
       ),
 
